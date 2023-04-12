dbeeda205e97c7b71f969f6a71cfa174075c0d5f
bonghee <hae8064@naver.com>
2023-04-12 21:39:00 +0900

    [Design] header and footer layout

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <p className="bg-slate-900 text-white py-2 text-sm text-center">
+      Copyright 2023. 이봉희 All Right Reserved.
+    </p>
+  );
+}
